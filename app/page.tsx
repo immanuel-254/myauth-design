b7e7419e86812859d00937f5766ad37e18cbdab5
immanuel-254 <njugunaimmanuel1@outlook.com>
2025-02-24 10:00:06 +0300

    added a toggle button for changing theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import ChangePasswordForm from "./components/change-password";
 import LoginForm from "./components/login";
 import LogoutForm from "./components/logout";
+import { ModeToggle } from "./components/moddle-toggler";
 import NavBar from "./components/navbar";
 import ResetPasswordForm from "./components/reset-password";
 
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div>
       <NavBar/>
+      <ModeToggle/>
       <LoginForm/>
       <hr />
       <ChangePasswordForm/>
